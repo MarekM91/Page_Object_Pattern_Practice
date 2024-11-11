406ad689964cd77462d430bb37c802ef044a4da4
Michal Marek <marekmichal1991@gmail.com>
2024-11-11 17:05:27 +0100

    Add project files.

---
+++ Page_Object_Pattern_Practice/Pages/Page.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+
+
+namespace Page_Object_Pattern_Practice.Pages
+{
+    internal class Page
+    {
+        private static string Url { get; } = "https://pastebin.com/";
+
+        private readonly IWebDriver driver;
+
+        public Page(IWebDriver driver) => this.driver = driver ?? throw new ArgumentException(nameof(driver));
+
+        public Page Open()
+        {
+            driver.Url = Url;
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return this;
+        }
+
+        public void Paste(string text, string name)
+        {
+            IWebElement cookiesButton = driver.FindElement(By.XPath("//button[span[text()='AGREE']]"));
+            cookiesButton.Click();
+
+            IWebElement newPasteField = driver.FindElement(By.Id("postform-text"));
+            newPasteField.Click();
+            newPasteField.SendKeys(text);
+
+            IWebElement pasteExpirationField = driver.FindElement(By.Id("select2-postform-expiration-container"));
+            pasteExpirationField.Click();
+
+            IWebElement highlightedExpirationOption = driver.FindElement(By.XPath("//li[contains(@class, 'select2-results__option') and text()='10 Minutes']"));
+            highlightedExpirationOption.Click();
+
+            IWebElement pasteName = driver.FindElement(By.Id("postform-name"));
+            pasteName.Click();
+            pasteName.SendKeys(name);
+
+            IWebElement savePaste = driver.FindElement(By.XPath("//button[text()='Create New Paste']"));
+            savePaste.Click();
+        }
+    }
+}
---
+++ Page_Object_Pattern_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Page_Object_Pattern_Practice.Pages;
+
+IWebDriver driver = new ChromeDriver();
+
+try
+{
+    var page = new Page(driver);
+
+    page.Open().Paste("Hello from WebDriver", "helloweb");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    driver.Quit();
+}
